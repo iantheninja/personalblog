05e492097ae61867f5ecdd2cad58aa5c11b340cb
iantheninja <munene.ian@live.com>
2015-09-29 12:17:01 +0300

    show method added to controller, show html file added

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,21 @@ class PostsController < ApplicationController
     #@posts = Post.all
   end
   def new
-    
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.save
+
+    redirect_to @post
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 end
