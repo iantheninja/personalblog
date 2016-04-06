03f05bac1b3ef154822c60942ca7a421f89fdf48
iantheninja <munene.ian@live.com>
2016-04-06 14:36:12 +0300

    working on disqus comments

---
+++ config/initializers/disqus_rails.rb
@@ -0,0 +1,6 @@
+DisqusRails.setup do |config|
+  config::SHORT_NAME = "your_short_name"
+  config::SECRET_KEY = "your_secret_disqus_key" #leave blank if not used
+  config::PUBLIC_KEY = "your public_disqus_key" #leave blank if not used
+  config::ACCESS_TOKEN = "your_access_token" #you got it, right? ;-)
+end
