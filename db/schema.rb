6899a45e2e972af0dd785f1d3d7262ab576da94f
iantheninja <munene.ian@live.com>
2016-05-13 13:31:47 +0300

    tagging and tag model created

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160505210110) do
+ActiveRecord::Schema.define(version: 20160513103032) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,4 +45,22 @@ ActiveRecord::Schema.define(version: 20160505210110) do
     t.datetime "image_updated_at"
   end
 
+  create_table "taggings", force: :cascade do |t|
+    t.integer  "post_id"
+    t.integer  "tag_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "taggings", ["post_id"], name: "index_taggings_on_post_id", using: :btree
+  add_index "taggings", ["tag_id"], name: "index_taggings_on_tag_id", using: :btree
+
+  create_table "tags", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "taggings", "posts"
+  add_foreign_key "taggings", "tags"
 end
