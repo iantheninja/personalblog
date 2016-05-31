a529771f2d0e2ecbe24488d1e056fd915723ddf5
iantheninja <munene.ian@live.com>
2016-05-31 11:42:31 +0300

    migration error removed, minor changes in styling

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160516144643) do
+ActiveRecord::Schema.define(version: 20160527084953) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,7 +34,6 @@ ActiveRecord::Schema.define(version: 20160516144643) do
 
   add_index "admins", ["email"], name: "index_admins_on_email", unique: true, using: :btree
   add_index "admins", ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true, using: :btree
-  add_index "admins", ["username"], name: "index_admins_on_username", unique: true, using: :btree
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
