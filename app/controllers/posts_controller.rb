ca0696e6aa373b910851df1c1b00fd37d8d2945b
iantheninja <munene.ian@live.com>
2016-06-02 01:11:23 +0300

    dependent destroy added to related models to prevent errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,7 +9,7 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new(post_params)
+    @post = current_admin.posts.build(post_params)
     if @post.save
       redirect_to @post
     else
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
-  has_many  :taggings
-  has_many :tags, through: :taggings
+  has_many  :taggings, dependent: :destroy
+  has_many :tags, through: :taggings, dependent: :destroy
   belongs_to :admin
   has_attached_file :image, styles: { medium: "400x400>" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/ #makes sure an image is uploaded
