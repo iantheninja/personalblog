65f4eeae52938ae0bbe7387c841d0c83da78e0fb
iantheninja <munene.ian@live.com>
2016-06-08 23:01:28 +0300

    extra images removed, image_tag in show page altered

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@
   has_many :tags, through: :taggings, dependent: :destroy
   belongs_to :admin
   has_attached_file :image, styles: { medium: "400x400>" }
-  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/ #makes sure an image is uploaded
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/ #makes sure it is an image
 
   validates :title, presence: true, length: {minimum: 5}
   validates :body, presence: true
