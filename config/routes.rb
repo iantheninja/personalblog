ccf11bcc3dc2e0ee22baba3fbd85399659337de2
iantheninja <munene.ian@live.com>
2016-06-10 19:47:42 +0300

    home button added to show pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  resources :posts do
-    resources :comments
+  #devise_for :admins
+  resources :posts
+
+  devise_for :admins do
+    # get 'admins/exit', to: 'devise/sessions#destroy', as: :logout
   end
   root "posts#index"
 end
